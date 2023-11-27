094463853c43e3cf8414bc83009765fd4e4bfe73
pekkatii <128123475+pekkatii@users.noreply.github.com>
2023-11-27 11:27:46 +0200

    Update test_process_a_study.R

--- tests/testthat/test_process_a_study.R
+++ tests/testthat/test_process_a_study.R
@@ -10,7 +10,8 @@ test_that("process_a_study", {
     subjects = tsoa_data$subjects,
     parameters = tsoa_data$parameters,
     custom_timeseries = tsoa_data$custom_timeseries,
-    timeseries_features_to_calculate = c(
+    custom_reference_groups = tsoa_data$custom_reference_groups,
+    default_timeseries_features_to_calculate = c(
       "autocorr",
       "average",
       "own_site_simil_score",
@@ -53,8 +54,8 @@ test_that("process_a_study", {
 test_that("process_a_study - optimize_sites_and_patients, backwards compatibility", {
 
   data("tsoa_data", package = "tsoa")
-
-  # only keep the first 25 percent of timepoint ranks for site 1
+  
+  # only keep the first 25 percent of timepoint ranks for site AAA
   tsoa_data$data <- tsoa_data$data %>%
     left_join(
       tsoa_data$subjects %>%
@@ -62,19 +63,20 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
       by = "subject_id"
     ) %>%
     filter(
-      site != "1" | percent_rank(timepoint_rank) <= 0.25,
+      site != "AAA" | percent_rank(timepoint_rank) <= 0.25,
       .by = "site"
     ) %>%
     select(-site)
-
-  # in default we expect site 1 which has only patients with
+  
+  # in default we expect site AAA which has only patients with
   # a low number of patients to get dropped from the analysis
   ls_tsoa_default <- process_a_study(
     data = tsoa_data$data,
     subjects = tsoa_data$subjects,
     parameters = tsoa_data$parameters,
     custom_timeseries = tsoa_data$custom_timeseries,
-    timeseries_features_to_calculate = c(
+    custom_reference_groups = tsoa_data$custom_reference_groups,
+    default_timeseries_features_to_calculate = c(
       "autocorr",
       "average",
       "own_site_simil_score",
@@ -87,15 +89,16 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
     default_generate_change_from_baseline = FALSE,
     autogenerate_timeseries = TRUE
   )
-
+  
   # if optimize_sites_and_patients an additional timeseries will be created
-  # that includes the maximum number of sites and patients thus keeping site 1
+  # that includes the maximum number of sites and patients thus keeping site AAA
   ls_tsoa_opt <- process_a_study(
     data = tsoa_data$data,
     subjects = tsoa_data$subjects,
     parameters = tsoa_data$parameters,
     custom_timeseries = tsoa_data$custom_timeseries,
-    timeseries_features_to_calculate = c(
+    custom_reference_groups = tsoa_data$custom_reference_groups,
+    default_timeseries_features_to_calculate = c(
       "autocorr",
       "average",
       "own_site_simil_score",
@@ -109,9 +112,9 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
     autogenerate_timeseries = TRUE,
     optimize_sites_and_patients = TRUE
   )
-
-  expect_true(! "1" %in% ls_tsoa_default$site_scores$site)
-  expect_true("1" %in% ls_tsoa_opt$site_scores$site)
+  
+  expect_true(! "AAA" %in% ls_tsoa_default$site_scores$site)
+  expect_true("AAA" %in% ls_tsoa_opt$site_scores$site)
   expect_true(all(ls_tsoa_default$timeseries$timepoint_combo %in% ls_tsoa_opt$timeseries$timepoint_combo))
   expect_true(! all(ls_tsoa_opt$timeseries$timepoint_combo %in% ls_tsoa_default$timeseries$timepoint_combo))
 
