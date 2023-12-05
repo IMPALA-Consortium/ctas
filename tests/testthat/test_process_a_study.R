4d5e4fff4018eb0cbf6dfbc155217394af761b8b
pekkatii <128123475+pekkatii@users.noreply.github.com>
2023-12-05 14:32:04 +0200

    Update test_process_a_study.R

--- tests/testthat/test_process_a_study.R
+++ tests/testthat/test_process_a_study.R
@@ -13,14 +13,14 @@ feats_collapse = paste(feats, collapse = ";")
 
 test_that("process_a_study", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
-  ls_tsoa <- process_a_study(
-    data = tsoa_data$data,
-    subjects = tsoa_data$subjects,
-    parameters = tsoa_data$parameters,
-    custom_timeseries = tsoa_data$custom_timeseries,
-    custom_reference_groups = tsoa_data$custom_reference_groups,
+  ls_ctas <- process_a_study(
+    data = ctas_data$data,
+    subjects = ctas_data$subjects,
+    parameters = ctas_data$parameters,
+    custom_timeseries = ctas_data$custom_timeseries,
+    custom_reference_groups = ctas_data$custom_reference_groups,
     default_timeseries_features_to_calculate = feats_collapse,
     default_minimum_timepoints_per_series = 3,
     default_minimum_subjects_per_series = 3,
@@ -29,20 +29,20 @@ test_that("process_a_study", {
     autogenerate_timeseries = TRUE
   )
 
-  expected_tsoa_names <- c(
+  expected_ctas_names <- c(
     "site_scores",
     "PCA_coordinates",
     "timeseries_features",
     "timeseries"
   )
 
-  expect_true(all(feats %in% unique(ls_tsoa$site_scores$feature)))
+  expect_true(all(feats %in% unique(ls_ctas$site_scores$feature)))
 
-  expect_true(all(expected_tsoa_names %in%  names(ls_tsoa)))
+  expect_true(all(expected_ctas_names %in%  names(ls_ctas)))
 
   expect_true(all(unlist(
     purrr::map(
-      ls_tsoa,
+      ls_ctas,
       ~ ! any(is.na(.)
         ))
       )
@@ -50,7 +50,7 @@ test_that("process_a_study", {
 
   expect_true(all(unlist(
     purrr::map(
-      ls_tsoa,
+      ls_ctas,
       ~ nrow(.) > 0
     )
   )))
@@ -59,12 +59,12 @@ test_that("process_a_study", {
 
 test_that("process_a_study - optimize_sites_and_patients, backwards compatibility", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
   # only keep the first 25 percent of timepoint ranks for site AAA
-  tsoa_data$data <- tsoa_data$data %>%
+  ctas_data$data <- ctas_data$data %>%
     left_join(
-      tsoa_data$subjects %>%
+      ctas_data$subjects %>%
         select(subject_id, site),
       by = "subject_id"
     ) %>%
@@ -76,12 +76,12 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
 
   # in default we expect site AAA which has only patients with
   # a low number of patients to get dropped from the analysis
-  ls_tsoa_default <- process_a_study(
-    data = tsoa_data$data,
-    subjects = tsoa_data$subjects,
-    parameters = tsoa_data$parameters,
-    custom_timeseries = tsoa_data$custom_timeseries,
-    custom_reference_groups = tsoa_data$custom_reference_groups,
+  ls_ctas_default <- process_a_study(
+    data = ctas_data$data,
+    subjects = ctas_data$subjects,
+    parameters = ctas_data$parameters,
+    custom_timeseries = ctas_data$custom_timeseries,
+    custom_reference_groups = ctas_data$custom_reference_groups,
     default_timeseries_features_to_calculate = feats_collapse,
     default_minimum_timepoints_per_series = 3,
     default_minimum_subjects_per_series = 3,
@@ -93,12 +93,12 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
   # if optimize_sites_and_patients an additional timeseries will be created
   # that includes the maximum number of sites and patients thus keeping site AAA
   expect_warning(
-      ls_tsoa_opt <- process_a_study(
-      data = tsoa_data$data,
-      subjects = tsoa_data$subjects,
-      parameters = tsoa_data$parameters,
-      custom_timeseries = tsoa_data$custom_timeseries,
-      custom_reference_groups = tsoa_data$custom_reference_groups,
+      ls_ctas_opt <- process_a_study(
+      data = ctas_data$data,
+      subjects = ctas_data$subjects,
+      parameters = ctas_data$parameters,
+      custom_timeseries = ctas_data$custom_timeseries,
+      custom_reference_groups = ctas_data$custom_reference_groups,
       default_timeseries_features_to_calculate = feats_collapse,
       default_minimum_timepoints_per_series = 3,
       default_minimum_subjects_per_series = 3,
@@ -110,31 +110,31 @@ test_that("process_a_study - optimize_sites_and_patients, backwards compatibilit
     regexp = "NA in dist object returning NA for lof"
   )
 
-  expect_true(! "AAA" %in% ls_tsoa_default$site_scores$site)
-  expect_true("AAA" %in% ls_tsoa_opt$site_scores$site)
-  expect_true(all(ls_tsoa_default$timeseries$timepoint_combo %in% ls_tsoa_opt$timeseries$timepoint_combo))
-  expect_true(! all(ls_tsoa_opt$timeseries$timepoint_combo %in% ls_tsoa_default$timeseries$timepoint_combo))
+  expect_true(! "AAA" %in% ls_ctas_default$site_scores$site)
+  expect_true("AAA" %in% ls_ctas_opt$site_scores$site)
+  expect_true(all(ls_ctas_default$timeseries$timepoint_combo %in% ls_ctas_opt$timeseries$timepoint_combo))
+  expect_true(! all(ls_ctas_opt$timeseries$timepoint_combo %in% ls_ctas_default$timeseries$timepoint_combo))
 
 })
 
 
 test_that("process_a_study - default_max_share_missing_timepoints_per_series <- 0", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
-  tsoa_data$data <- tsoa_data$data %>%
+  ctas_data$data <- ctas_data$data %>%
     mutate(
       result = ifelse(is.na(result), mean(result, na.rm = TRUE), result),
       # only timepoints that are not null for at least one patient are considered
       result = ifelse(timepoint_rank == 1 & subject_id != "1", NA, result)
     )
 
-  ls_tsoa <- process_a_study(
-    data = tsoa_data$data,
-    subjects = tsoa_data$subjects,
-    parameters = tsoa_data$parameters,
-    custom_timeseries = tsoa_data$custom_timeseries,
-    custom_reference_groups = tsoa_data$custom_reference_groups,
+  ls_ctas <- process_a_study(
+    data = ctas_data$data,
+    subjects = ctas_data$subjects,
+    parameters = ctas_data$parameters,
+    custom_timeseries = ctas_data$custom_timeseries,
+    custom_reference_groups = ctas_data$custom_reference_groups,
     default_timeseries_features_to_calculate = feats_collapse,
     default_minimum_timepoints_per_series = 1,
     default_minimum_subjects_per_series = 3,
@@ -143,20 +143,20 @@ test_that("process_a_study - default_max_share_missing_timepoints_per_series <- 
     autogenerate_timeseries = TRUE
   )
 
-  expect_true(all(purrr::map_lgl(ls_tsoa, is.null)))
+  expect_true(all(purrr::map_lgl(ls_ctas, is.null)))
 
 })
 
 test_that("process_a_study - default_minimum_timepoints_per_series <- 1e6", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
-  ls_tsoa <- process_a_study(
-    data = tsoa_data$data,
-    subjects = tsoa_data$subjects,
-    parameters = tsoa_data$parameters,
-    custom_timeseries = tsoa_data$custom_timeseries,
-    custom_reference_groups = tsoa_data$custom_reference_groups,
+  ls_ctas <- process_a_study(
+    data = ctas_data$data,
+    subjects = ctas_data$subjects,
+    parameters = ctas_data$parameters,
+    custom_timeseries = ctas_data$custom_timeseries,
+    custom_reference_groups = ctas_data$custom_reference_groups,
     default_timeseries_features_to_calculate = feats_collapse,
     default_minimum_timepoints_per_series = 1e6,
     default_minimum_subjects_per_series = 3,
@@ -165,20 +165,20 @@ test_that("process_a_study - default_minimum_timepoints_per_series <- 1e6", {
     autogenerate_timeseries = TRUE
   )
 
-  expect_true(all(purrr::map_lgl(ls_tsoa, is.null)))
+  expect_true(all(purrr::map_lgl(ls_ctas, is.null)))
 
 })
 
 test_that("process_a_study - default_minimum_subjects_per_series <- 1e6", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
-  ls_tsoa <- process_a_study(
-    data = tsoa_data$data,
-    subjects = tsoa_data$subjects,
-    parameters = tsoa_data$parameters,
-    custom_timeseries = tsoa_data$custom_timeseries,
-    custom_reference_groups = tsoa_data$custom_reference_groups,
+  ls_ctas <- process_a_study(
+    data = ctas_data$data,
+    subjects = ctas_data$subjects,
+    parameters = ctas_data$parameters,
+    custom_timeseries = ctas_data$custom_timeseries,
+    custom_reference_groups = ctas_data$custom_reference_groups,
     default_timeseries_features_to_calculate = feats_collapse,
     default_minimum_timepoints_per_series = 1,
     default_minimum_subjects_per_series = 1e6,
@@ -187,30 +187,30 @@ test_that("process_a_study - default_minimum_subjects_per_series <- 1e6", {
     autogenerate_timeseries = TRUE
   )
 
-  expect_true(all(purrr::map_lgl(ls_tsoa, is.null)))
+  expect_true(all(purrr::map_lgl(ls_ctas, is.null)))
 
 })
 
 test_that("process_a_study - default_minimum_subjects_per_series <- 1", {
 
-  data("tsoa_data", package = "tsoa")
+  data("ctas_data", package = "ctas")
 
-  df_lonely <- tsoa_data$data %>%
+  df_lonely <- ctas_data$data %>%
     filter(subject_id == "1", parameter_id == "param1") %>%
     mutate(timepoint_rank = timepoint_rank + max(timepoint_rank))
 
-  tsoa_data$data <- bind_rows(
-    tsoa_data$data,
+  ctas_data$data <- bind_rows(
+    ctas_data$data,
     df_lonely
   )
 
   expect_warning(
-    ls_tsoa <- process_a_study(
-      data = tsoa_data$data,
-      subjects = tsoa_data$subjects,
-      parameters = tsoa_data$parameters,
-      custom_timeseries = tsoa_data$custom_timeseries,
-      custom_reference_groups = tsoa_data$custom_reference_groups,
+    ls_ctas <- process_a_study(
+      data = ctas_data$data,
+      subjects = ctas_data$subjects,
+      parameters = ctas_data$parameters,
+      custom_timeseries = ctas_data$custom_timeseries,
+      custom_reference_groups = ctas_data$custom_reference_groups,
       default_timeseries_features_to_calculate = feats_collapse,
       default_minimum_timepoints_per_series = 1,
       default_minimum_subjects_per_series = 1,
@@ -221,9 +221,9 @@ test_that("process_a_study - default_minimum_subjects_per_series <- 1", {
     "NA in dist object returning NA for lof"
   )
 
-  expect_true(! any(purrr::map_lgl(ls_tsoa, is.null)))
-  expect_true(! any(purrr::map_lgl(ls_tsoa, ~ nrow(.) == 0)))
-  expect_true(min(ls_tsoa$site_scores$subject_count) == 1)
+  expect_true(! any(purrr::map_lgl(ls_ctas, is.null)))
+  expect_true(! any(purrr::map_lgl(ls_ctas, ~ nrow(.) == 0)))
+  expect_true(min(ls_ctas$site_scores$subject_count) == 1)
 
 })
 
